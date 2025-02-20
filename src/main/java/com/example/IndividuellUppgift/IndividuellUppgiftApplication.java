57ff69e97a27e16287103f8788c565a83db1bf09
chihiroj <77557825+chihiroj@users.noreply.github.com>
2025-02-20 09:04:16 +0100

    Initial commit

---
+++ src/main/java/com/example/IndividuellUppgift/IndividuellUppgiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.IndividuellUppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividuellUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividuellUppgiftApplication.class, args);
+	}
+
+}
